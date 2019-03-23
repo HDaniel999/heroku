f8c7aa84655062c4cb4fab508a0e4386a650cf2a
HDaniel999 <hdaniel999@gmail.com>
2019-03-22 21:06:16 -0600

    Require Mongoose with connection

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const config = require('./config')
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
+mongoose.connect(config.database_url, { useNewUrlParser: true})
+  .then (
+    () => {
+      console.log('Database Ready for Action')
+    },
+    err => {
+      console.error(err)
+    }
+  )
 app.all('/', (req, res) => {
   res.json({
     api: 'Heroku API!!',
